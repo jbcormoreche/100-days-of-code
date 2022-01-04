0294ba8f4037476685d63e80e6247154eb394050
JB Cormoreche <jbcdu69@hotmail.fr>
2022-01-04 13:47:02 +0100

    Update Day003-Control_Flow-Logical_Operators-Treasure_Island.py

--- Day003-Control_Flow-Logical_Operators-Treasure_Island.py
+++ Day003-Control_Flow-Logical_Operators-Treasure_Island.py
@@ -21,6 +21,7 @@ else:
 # == checks equality
 
 # Interactive Coding Exercise - Odd or Even
+# Write a program that works out whether a given number is an odd or even number.
 number = int(input("Which number do you want to check? "))
 if number % 2 == 0:
     print("This is an even number.")
