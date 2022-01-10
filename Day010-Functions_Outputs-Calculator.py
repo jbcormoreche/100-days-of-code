cf6581448a79520744c0805ad1a8140ee6233f71
JB Cormoreche <jbcdu69@hotmail.fr>
2022-01-10 15:54:56 +0100

    Add Day010-Functions_Outputs-Calculator.py

---
+++ Day010-Functions_Outputs-Calculator.py
@@ -0,0 +1,116 @@
+# Functions with outputs
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+print(format_name(first_name, last_name))
+
+output = len("Angela")
+
+# Interactive Coding Exercise - Days in Month
+# Write a program that works out the number of days in a month.
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month"
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    else:
+        return month_days[month - 1]
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month (from 1 to 12): "))
+days = days_in_month(year, month)
+print(f"There are {days} days in this month.")
+
+# Docstrings: string literals that appear right after the definition of a function, method, class, or module (hover over function)
+def format_name(f_name, l_name):
+    """"Take a first and last name and format it to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+output = len("Angela")
+
+# Recursion: function that calls itself
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 3
+print("The factorial of", num, "is", factorial(num))
+
+# Day 10 Project - Calculator
+from calculator_art import logo
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def substract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    should_continue = True
+    while should_continue:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
