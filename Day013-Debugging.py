b692347abcb0488e10f498ab1aaba0fac1986050
JB Cormoreche <jbcdu69@hotmail.fr>
2022-01-13 14:16:55 +0100

    Add Day013-Debugging.py

---
+++ Day013-Debugging.py
@@ -0,0 +1,21 @@
+# Debugging
+
+# Understand the system
+# Read the documentation/manual
+# Describe the problem (Rubber Duck Debugging)
+# Reproduce the bug, make it fail
+# Evaluate each line
+# Question your assumptions
+# Fix errors until the problem is fixed
+# Change one thing at a time
+# Be methodical
+# Track what you have already tried so you don't revisit the same thing over and over
+# Write it down or put temporary comments in the code
+# Watch for underlines indicating problems
+# Squash bugs with a print() statement
+# Run your code often to catch bugs early on
+# Use a debugger
+# Copy the error and do some research online
+# Ask for help on a platform or a forum
+# Take a break, get a fresh view
+# Be persistent and get after it
