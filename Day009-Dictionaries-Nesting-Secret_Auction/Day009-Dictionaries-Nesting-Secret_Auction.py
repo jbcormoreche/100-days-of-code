3257962ad1e89780c0a265c536e1fe20aa738ddd
JB Cormoreche <jbcdu69@hotmail.fr>
2022-01-28 14:29:55 +0100

    Update Day009-Dictionaries-Nesting-Secret_Auction.py

--- Day009-Dictionaries-Nesting-Secret_Auction/Day009-Dictionaries-Nesting-Secret_Auction.py
+++ Day009-Dictionaries-Nesting-Secret_Auction/Day009-Dictionaries-Nesting-Secret_Auction.py
@@ -101,6 +101,7 @@ travel_log = [
     },
 ]
 
+
 def add_new_country(country_visited, times_visited, cities_visited):
     new_country = {}
     new_country["country"] = country_visited
@@ -111,6 +112,7 @@ def add_new_country(country_visited, times_visited, cities_visited):
 
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
+
 
 # Other solution
 def add_new_country(country_visited, times_visited, cities_visited):
