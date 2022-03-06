17fccb204b8751f15ef66aa75b7be2e41d4d5e11
JB Cormoreche <jbcdu69@hotmail.fr>
2022-03-06 12:45:42 +0100

    Add Day065-Web_Design_Introduction

---
+++ Day065-Web_Design_Introduction/Day065-Web_Design_Introduction.py
@@ -0,0 +1,21 @@
+# Web Design Introduction
+
+# Color Theory
+# Create the right palette of colors for your website/product
+# Color harmony rules: analogous, monochromatic, triad, complementary, square, shades...
+
+# Typography
+# Font Families: serif, san-serif, script, display, modern...
+# Stick to two fonts for one design
+
+# User Interface (UI)
+# Hierarchy through color, size, layout...
+# Pay attention to consistency, alignment and spacing (use grid lines)
+# Design for the right audience
+
+# User Experience (UX)
+# Keep it simple
+# Keep your design and functionality of your website/product consistent
+# Reading Patterns: F-layout, Z-layout
+# Design for all platforms, make it responsive
+# Test with users to get feedback and adjust if necessary
